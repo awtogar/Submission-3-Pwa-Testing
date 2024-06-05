fbe6355f5ea4dc7e89ea72944c8705ae508e9ac3
garebizzare <awaludintogar@gmail.com>
2024-06-05 23:20:05 +0700

    lil error

---
+++ sharp.js
@@ -0,0 +1,55 @@
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
+
+const target = path.resolve(__dirname, 'src/public/images');
+const destination = path.resolve(__dirname, 'src/public/images');
+
+if (!fs.existsSync(destination)) {
+    fs.mkdirSync(destination);
+}
+
+fs.readdirSync(target).forEach((image) => {
+    sharp(`${target}/${image}`)
+        .resize(1200)
+        .toFile(
+            path.resolve(
+                __dirname,
+                `${destination}/${image.split('.').slice(0, -1).join('.')}-extra-large.jpg`,
+            ),
+        );
+
+    sharp(`${target}/${image}`)
+        .resize(1024)
+        .toFile(
+            path.resolve(
+                __dirname,
+                `${destination}/${image.split('.').slice(0, -1).join('.')}-large.jpg`,
+            ),
+        );
+
+    sharp(`${target}/${image}`)
+        .resize(768)
+        .toFile(
+            path.resolve(
+                __dirname,
+                `${destination}/${image.split('.').slice(0, -1).join('.')}-medium.jpg`,
+            ),
+        );
+    sharp(`${target}/${image}`)
+        .resize(368)
+        .toFile(
+            path.resolve(
+                __dirname,
+                `${destination}/${image.split('.').slice(0, -1).join('.')}-small.jpg`,
+            ),
+        );
+    sharp(`${target}/${image}`)
+        .resize(200)
+        .toFile(
+            path.resolve(
+                __dirname,
+                `${destination}/${image.split('.').slice(0, -1).join('.')}-extra-small.jpg`,
+            ),
+        );
+});
--- webpack.common.js
+++ webpack.common.js
@@ -3,6 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
+const ImageminMozjpeg = require('imagemin-mozjpeg');
+
 
 module.exports = {
   entry: {
@@ -35,6 +38,10 @@ module.exports = {
         {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
+          globOptions: {
+            // CopyWebpackPlugin mengabaikan berkas yang berada di dalam folder images
+            ignore: ['**/images/heros/**'],
+          },
         },
       ],
     }),
@@ -55,6 +62,14 @@ module.exports = {
             cacheName: "restaurant-cache",
           },
         },
+      ],
+    }),
+    new ImageminWebpackPlugin({
+      plugins: [
+        ImageminMozjpeg({
+          quality: 50,
+          progressive: true,
+        }),
       ],
     }),
   ],
